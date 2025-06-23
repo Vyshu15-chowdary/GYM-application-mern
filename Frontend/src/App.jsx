cd68966788a45bcc8374a7ffe1dc05df350ee661
vyshnavi <vishuchowdary20@gmail.com>
2025-06-23 14:02:50 +0530

    imported app.css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {BrowserRouter as Router} from "react-router-dom"
-
+import "./App.css";
 import React from 'react'
 
 const App = () => {
