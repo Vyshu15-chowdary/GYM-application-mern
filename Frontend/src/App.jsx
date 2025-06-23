728c9a8d7883346977502549b3df632191c0c68f
vyshnavi <vishuchowdary20@gmail.com>
2025-06-23 12:46:45 +0530

    components added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,1 +1,24 @@
+import React from 'react'
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {BrowserRouter as Router} from "react-router-dom"
 
+import React from 'react'
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar/>
+      <Hero/>
+      <workoutSessions/>
+      <Gallery/>
+      <Pricing/>
+      <Contact/>
+      <BMICaluculator/>
+      <Footer/>
+
+    </Router>
+  )
+}
+
+export default App
