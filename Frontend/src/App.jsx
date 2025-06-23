f4400ed3dfc1e2cb4f8985322e9a9c4da9ed376a
vyshnavi <vishuchowdary20@gmail.com>
2025-06-23 14:26:58 +0530

    pricing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,13 +4,18 @@ import 'react-toastify/dist/ReactToastify.css';
 import {BrowserRouter as Router} from "react-router-dom"
 import "./App.css";
 import React from 'react'
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import WorkoutSessions from "./components/Workoutsession"
+import Gallery from "./components/Gallery"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <Router>
       <Navbar/>
       <Hero/>
-      <workoutSessions/>
+      <WorkoutSessions/>
       <Gallery/>
       <Pricing/>
       <Contact/>
