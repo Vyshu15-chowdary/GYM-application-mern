2f8b20b1a43f8bc7bae8f22b8358abad035a4d71
vyshnavi <vishuchowdary20@gmail.com>
2025-06-24 16:53:45 +0530

    crreating gallery component

--- Frontend/src/components/Gallery.jsx
+++ Frontend/src/components/Gallery.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
+  const gallery = [
+    "/img1.webp",
+    "/img2.jpg",
+    "/img3.jpg",
+    "/img4.webp",
+    "/img5.webp",
+    "/img6.jpg",
+    "/img7.webp",
+    "/img8.jpg"]
+  return <section className='gallery'>
+    <h1>BETTER BEATS BEST</h1>
+    <div className='images'>
+      <div>
+        {
+          gallery.slice(0,3).map((element,index) =>(
+            <img key={index} src={element} alt="galleryImage"/>
+          ))
+        }
+      </div>
+
+    </div>
+
+  </section>
 }
 
 export default Gallery
