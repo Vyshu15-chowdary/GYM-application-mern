b2029feb0e376a9fe884b33640946d07be91912f
vyshnavi <vishuchowdary20@gmail.com>
2025-06-24 20:59:09 +0530

    working on pricing page

--- Frontend/src/components/Pricing.jsx
+++ Frontend/src/components/Pricing.jsx
@@ -1,8 +1,80 @@
 import React from 'react'
+import {check} from "lucide-react"
 
 const Pricing = () => {
+  const pricing = [
+    {
+      imgUrl : "/pricing.webp",
+      title:"QUATERLY",
+      price : 18000,
+      length : 3
+    },
+
+     {
+      imgUrl : "/pricing.webp",
+      title:"HALF_YEARLY",
+      price : 34000,
+      length : 6
+    },
+
+     {
+      imgUrl : "/pricing.webp",
+      title:"YEARLY",
+      price : 67000,
+      length : 12
+    }
+  ]
   return (
-    <div>Pricing</div>
+    <>
+    <section className='pricing'>
+      <h1>ELEVATE FITNESS GYM PLANS</h1>
+     <div className='wrapper'>
+      {
+
+        pricing.map(element =>{
+          return(
+            <div className='card' key = {element.title}>
+              <img src ={element}  alt={element.title} />
+              <div className='title'>
+                <h1>{element.title}</h1>
+                <h1>PACKAGE</h1>
+                <h1>Rs{element.price}</h1>
+                <p>For {element.length}</p>
+
+              </div>
+              <div className="description">
+                <p>
+                  <check/>Equipment
+                </p>
+
+                <p>
+                  <check/>All Day Free Training
+                </p>
+
+                <p>
+                  <check/>Free Restroom
+                </p>
+
+                <p>
+                  <check/>24/7 Skilled Support
+                </p>
+
+                <p>
+                  <check/>20 Days Freezing Option
+                </p>
+              </div>
+
+              </div>
+          )
+        })
+      }
+
+
+     </div>
+     
+    </section>
+    </>
+    
   )
 }
 
