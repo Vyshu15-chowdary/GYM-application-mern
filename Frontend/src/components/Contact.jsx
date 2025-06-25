1ac4613e12a02cf192a47c9f4e72050672dcf48f
vyshnavi <vishuchowdary20@gmail.com>
2025-06-25 13:17:48 +0530

    contact form created

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 
 const Contact = () => {
+  const Contact =()=>{
+    const[name,setName] = useState("");
+    const[email,setEmail] = useState("");
+    const[message,setMessage] = useState("");
+    const[loading,setLoading] =useState(false);
+
+
+  }
   return (
-    <div>Contact</div>
+    <section className='contact'>
+      <form>
+        <h1>CONTACT US</h1>
+        <div>
+          <label>Name  </label>
+          <input type = "text" 
+          value={name}
+          onChange={(e)=> setName(e.target.value)}/>
+        </div>
+
+         <div>
+          <label>Email </label>
+          <input type = "email"
+           value={name}
+          onChange={(e)=> setEmail(e.target.value)}/>
+           
+        </div>
+
+         <div>
+          <label>Message </label>
+          <input type = "text"
+           value={name}
+          onChange={(e)=> setMessage(e.target.value)}/> 
+        </div>
+
+        <button type="submit" disabled={loading}>
+          Send Message
+        </button>
+      </form>
+
+    </section>
   )
 }
 
