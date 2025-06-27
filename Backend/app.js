55d53ba63a87df0c900b9a3651f90e0226969b64
vyshnavi <vishuchowdary20@gmail.com>
2025-06-27 21:33:16 +0530

    working on cors

--- Backend/app.js
+++ Backend/app.js
@@ -4,3 +4,13 @@ import cors from "cors"
 
 const app = express();
 const router = express.Router();
+
+config({path: "./Config.env"});
+
+
+app.use(cors) //useful to connect backend and frontend
+
+app.listen(process.env.PORT,()=>{
+
+    console.log(`server listening stport ${process.env.PORT}`)
+});
