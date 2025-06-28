96a679f15286bdcb921df64ad22c4ae5a3b97346
vyshnavi <vishuchowdary20@gmail.com>
2025-06-28 20:39:04 +0530

    destructure of body ,error rectified

--- Backend/app.js
+++ Backend/app.js
@@ -1,50 +1,56 @@
-import express from "express"
-import {config}from "dotenv"
-import cors from "cors"
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import { sendEmail } from "./utils/sendEmail.js";
 
 const app = express();
 const router = express.Router();
 
-config({path: "./Config.env"});
+config({ path: "./config.env" });
 
-
-app.use(cors({
-    origin : [process.env.FRONTEND_URL],
-    methods:["POST"],
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST"],
     credentials: true,
-   
-        
-    
-})
-); //useful to connect backend and frontend
-
+  })
+);
 
 app.use(express.json());
-app.use(express.urlencoded ({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-router.use(router);
-router.post("/send/mail",async(req,res,next)=>{
-   const{name,email,message} = req.body;
-   if(!name || !email || !message){
-    return next(res.status(400).json({
-        sucess:false,
-        message:"Please provide all details"
-    }))
-   }
-   
-})
+router.post("/send/mail", async (req, res, next) => {
 
-router.get("/",(req,res,next)=>{
-
-    res.json({sucess:true,
-        message:"WELCOME TO MY PROJECT"
+  const { name, email, message } = req.body;  //destructuring of body
+  if (!name || !email || !message) {
+    return next(
+      res.status(400).json({
+        success: false,
+        message: "Please provide all details",
+      })
+    );
+  }
+  try {
+    await sendEmail({
+      email: "vishuchowdary20@gmail.com",
+      subject: "GYM WEBSITE CONTACT",
+      message,
+      userEmail: email,
     });
-
-})
+    res.status(200).json({
+      success: true,
+      message: "Message Sent Successfully.",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: " Internal Server Error",
+    });
+  }
+});
 
 app.use(router);
 
-app.listen(process.env.PORT,()=>{
-
-    console.log(`server listening stport ${process.env.PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at port ${process.env.PORT}`);
 });
